71c4c12eb9539940754954a4e182302d4fac115e
Joep Vanlier <joep.vanlier@fdm.uni-freiburg.de>
2015-09-15 13:26:40 +0200

    fix

--- Parser/parserDependencies/jacGen1.m
+++ Parser/parserDependencies/jacGen1.m
@@ -102,6 +102,7 @@ if ( fJac == 1 )
 
     disp( 'Replacing state names' );
     c_file = regexprep( c_file, 'MatrixWithNoName\[(\d*)\]', 'DENSE_COL(Jac,$1)' );
+    c_file = regexprep( c_file, 'A0\[(\d*)\]', 'DENSE_COL(Jac,$1)' );
     c_file = regexprep( c_file, 'p_(\d*)', 'data->p[$1]' );
     c_file = regexprep( c_file, 'u_(\d*)', 'data->u[$1]' );
     c_file = regexprep( c_file, 'x_(\d*)', 'stateVars[$1]' );
